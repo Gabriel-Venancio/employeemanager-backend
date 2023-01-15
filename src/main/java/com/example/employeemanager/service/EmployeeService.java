0a807786dde873ee0c70422162fada59f8c795c8
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2023-01-15 00:30:16 -0300

    EmployeeService findEmployeeById

--- src/main/java/com/example/employeemanager/service/EmployeeService.java
+++ src/main/java/com/example/employeemanager/service/EmployeeService.java
@@ -1,10 +1,13 @@
 package com.example.employeemanager.service;
 
+import com.example.employeemanager.exception.UserNotFoundException;
 import com.example.employeemanager.model.Employee;
 import com.example.employeemanager.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,5 +22,22 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepository.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found "));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteEmployeeById(id);
     }
 }
